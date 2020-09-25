8f2ff6f76249c05c1db1c3ce853e90be0ceb2b52
bolabcd <gaspar.artur@yahoo.com.br>
2020-09-25 09:32:16 -0300

    Renamed javaJSON library due to errors in Eclipse.

--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,6 @@
 module bestFilmFinder {
 	requires jdk.httpserver;
-	requires json.java;
+	requires jsonJava;
 	requires freemarker;
 	requires java.net.http;
 	exports com.bestFilmFinder.sourceWebInterfaces;
