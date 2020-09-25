27aaea83b86d59407db22dde5e05b936b8743a49
bolabcd <gaspar.artur@yahoo.com.br>
2020-09-25 09:38:13 -0300

    Added a short and a long description to Movie, removed web page URL (because we will use REST web APIs, not a raw web page), and added a list of locations in the Internet to watch the movie.

--- src/com/bestFilmFinder/sourceWebInterfaces/Movie.java
+++ src/com/bestFilmFinder/sourceWebInterfaces/Movie.java
@@ -1,63 +1,58 @@
 package com.bestFilmFinder.sourceWebInterfaces;
 
 import java.net.URL;
+import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 public class Movie {
 	private String name;
-	private URL imageURL;
-	private String description;
+	private String shortDescription;
+	private String longDescription;
 	private Map<String, Integer> scores;
-	private URL webpageURL;
-	private Set<String> genres;
-
+	private List<String> genres;
+	private URL imageURL;
+	private List<String> whereToWatch;
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
-	public URL getImageURL() {
-		return imageURL;
+	public String getShortDescription() {
+		return shortDescription;
 	}
-
-	public void setImageURL(URL imageURL) {
-		this.imageURL = imageURL;
+	public void setShortDescription(String shortDescription) {
+		this.shortDescription = shortDescription;
 	}
-
-	public String getDescription() {
-		return description;
+	public String getLongDescription() {
+		return longDescription;
 	}
-
-	public void setDescription(String description) {
-		this.description = description;
+	public void setLongDescription(String longDescription) {
+		this.longDescription = longDescription;
 	}
-
 	public Map<String, Integer> getScores() {
 		return scores;
 	}
-
 	public void setScores(Map<String, Integer> scores) {
 		this.scores = scores;
 	}
-
-	public URL getWebpageURL() {
-		return webpageURL;
-	}
-
-	public void setWebpageURL(URL webpageURL) {
-		this.webpageURL = webpageURL;
-	}
-
-	public Set<String> getGenres() {
+	public List<String> getGenres() {
 		return genres;
 	}
-
-	public void setGenres(Set<String> genres) {
+	public void setGenres(List<String> genres) {
 		this.genres = genres;
 	}
-
+	public URL getImageURL() {
+		return imageURL;
+	}
+	public void setImageURL(URL imageURL) {
+		this.imageURL = imageURL;
+	}
+	public List<String> getWhereToWatch() {
+		return whereToWatch;
+	}
+	public void setWhereToWatch(List<String> whereToWatch) {
+		this.whereToWatch = whereToWatch;
+	}
+	
 }
