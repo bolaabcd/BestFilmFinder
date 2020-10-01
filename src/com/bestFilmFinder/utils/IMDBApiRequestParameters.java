e9ae45453b4f627a628e6324f8490da882bb9c47
bolabcd <gaspar.artur@yahoo.com.br>
2020-10-01 11:49:24 -0300

    Added class to get all the IMDB requests required parameters.

---
+++ src/com/bestFilmFinder/utils/IMDBApiRequestParameters.java
@@ -0,0 +1,8 @@
+package com.bestFilmFinder.utils;
+
+public class IMDBApiRequestParameters {
+	public static final String APIKeyAsString="APIKey";
+	public static final String SearchExpressionAsString="SearchExpression";
+	public static final String LanguageAsString="Language";
+	public static final String SearchTypeAsString="SearchType";
+}
